5c636dff778670b55d12441734c167a3a20b1805
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-15 22:06:31 -0300

    (feat): start project

---
+++ website/forms.py
@@ -0,0 +1,26 @@
+from allauth.account.forms import SignupForm, LoginForm
+from django import forms
+
+
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget = forms.TextInput(attrs={"class": "input-form"})
+        self.fields["email"].widget = forms.HiddenInput()
+        self.fields["password1"].widget = forms.PasswordInput(
+            attrs={"class": "input-form"}
+        )
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].widget = forms.PasswordInput(
+            attrs={"class": "input-form"}
+        )
+        self.fields["password2"].help_text = ""
+
+
+class CustomLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["login"].widget = forms.TextInput(attrs={"class": "input-form"})
+        self.fields["password"].widget = forms.PasswordInput(
+            attrs={"class": "input-form"}
+        )
