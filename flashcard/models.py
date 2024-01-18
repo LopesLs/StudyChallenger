7b87054aa0441b7e3a161d03dab7da9021a29eac
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-18 19:29:32 -0300

    (feat): create models

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,19 +1,51 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Flashcard(models.Model):
-    DIFICULDADE_CHOICES = (('D', 'Difícil'), ('M', 'Médio'), ('F', 'Fácil'))
+    DIFICULDADE_CHOICES = (("D", "Difícil"), ("M", "Médio"), ("F", "Fácil"))
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     pergunta = models.CharField(max_length=100)
     resposta = models.TextField()
-    categoria = models.ForeignKey('Categoria', on_delete=models.DO_NOTHING)
+    categoria = models.ForeignKey("Categoria", on_delete=models.DO_NOTHING)
     dificuldade = models.CharField(max_length=1, choices=DIFICULDADE_CHOICES)
+
+    @property
+    def css_difficulty(self):
+        if self.dificuldade == "F":
+            return "flashcard-facil"
+        elif self.dificuldade == "M":
+            return "flashcard-medio"
+        elif self.dificuldade == "D":
+            return "flashcard-dificil"
 
     def __str__(self):
         return self.pergunta
+
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=20)
 
     def __str__(self):
         return self.nome
+
+
+class FlashcardDesafio(models.Model):
+    flashcard = models.ForeignKey("Flashcard", on_delete=models.DO_NOTHING)
+    respondido = models.BooleanField(default=False)
+    acertou = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.flashcard.pergunta
+
+
+class Desafio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=100)
+    categoria = models.ManyToManyField(Categoria)
+    quantidade_perguntas = models.IntegerField()
+    dificuldade = models.CharField(max_length=1, choices=Flashcard.DIFICULDADE_CHOICES)
+    flashcards = models.ManyToManyField(FlashcardDesafio)
+
+    def __str__(self):
+        return self.titulo
