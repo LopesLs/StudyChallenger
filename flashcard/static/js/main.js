f001ab80a6712f493c86078ad698023084c938b8
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-19 14:03:32 -0300

    (fix): script to hide and unhide container

--- flashcard/static/js/main.js
+++ flashcard/static/js/main.js
@@ -1,9 +1,9 @@
 function show_awnser(id) {
-    card = document.getElementById(id)
-
-    if (card.style.display == '') {
-        card.style.display = "block"
+    card = document.getElementById(id);
+    
+    if (card.style.display === 'none' || card.style.display === '') {
+        card.style.display = 'block';
     } else {
-        card.style.display = 'none'
+        card.style.display = "none";
     }
 }
