f5b13745464dad07d68538eb50ced8443e78670a
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-20 19:58:39 -0300

    (feat): Create CASCADE delete and chage returns

--- flashcard/models.py
+++ flashcard/models.py
@@ -4,20 +4,30 @@ from django.db import models
 
 class Flashcard(models.Model):
     DIFICULDADE_CHOICES = (("D", "Difícil"), ("M", "Médio"), ("F", "Fácil"))
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pergunta = models.CharField(max_length=100)
     resposta = models.TextField()
     categoria = models.ForeignKey("Categoria", on_delete=models.DO_NOTHING)
     dificuldade = models.CharField(max_length=1, choices=DIFICULDADE_CHOICES)
-
+    
     @property
     def css_difficulty(self):
         if self.dificuldade == "F":
-            return "flashcard-facil"
+            return ["success", "Fácil"]
         elif self.dificuldade == "M":
-            return "flashcard-medio"
+            return ["warning", "Médio"]
         elif self.dificuldade == "D":
-            return "flashcard-dificil"
+            return ["danger", "Difícil"]
+
+    def delete(self, *args, **kwargs):
+        desafios = Desafio.objects.filter(flashcards__flashcard=self)
+
+        for desafio in desafios:
+            print(desafio.flashcards.count())
+            if desafio.flashcards.count() <= 1:
+                raise Exception("Não é possível excluir o flashcard, pois ele está sendo utilizado em um desafio.")
+        
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.pergunta
@@ -31,7 +41,7 @@ class Categoria(models.Model):
 
 
 class FlashcardDesafio(models.Model):
-    flashcard = models.ForeignKey("Flashcard", on_delete=models.DO_NOTHING)
+    flashcard = models.ForeignKey("Flashcard", on_delete=models.CASCADE)
     respondido = models.BooleanField(default=False)
     acertou = models.BooleanField(default=False)
 
@@ -40,12 +50,13 @@ class FlashcardDesafio(models.Model):
 
 
 class Desafio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=100)
     categoria = models.ManyToManyField(Categoria)
     quantidade_perguntas = models.IntegerField()
     dificuldade = models.CharField(max_length=1, choices=Flashcard.DIFICULDADE_CHOICES)
     flashcards = models.ManyToManyField(FlashcardDesafio)
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo
