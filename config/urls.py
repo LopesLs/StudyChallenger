eca9b9b2212680a3ce4a687866f6929bf9699e62
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-19 21:00:12 -0300

    (feat):  media configs

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("website.urls")),
     path("flashcards/", include("flashcard.urls")),
+    path("apostilas/", include("apostilas.urls")),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
