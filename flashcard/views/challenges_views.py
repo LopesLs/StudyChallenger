99e07815f02cb913c4238ec3dee9e7ecaeb6f10c
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2024-01-23 18:08:34 -0300

    (refactor): Split views code

--- flashcard/views/challenges_views.py
+++ flashcard/views/challenges_views.py
@@ -4,53 +4,12 @@ from django.views.generic.edit import DeleteView
 from django.views.generic.list import ListView
 from django.shortcuts import redirect, render
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
 from django.http import HttpResponseForbidden
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-from .models import Flashcard, Categoria, Desafio, FlashcardDesafio
-from .forms import FlashcardForm, DesafioForm
-
-
-class FlashcardRequestHandler(LoginRequiredMixin, ListView):
-    model = Flashcard
-    template_name = "flashcard/flashcard_list.html"
-    context_object_name = "flashcards"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["categories"] = Categoria.objects.all()
-        context["dificulties"] = Flashcard.DIFICULDADE_CHOICES
-        return context
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        category = self.request.GET.get("category")
-        difficulty = self.request.GET.get("dificulty")
-
-        if category:
-            queryset = queryset.filter(categoria__id=category)
-
-        if difficulty:
-            queryset = queryset.filter(dificuldade=difficulty)
-
-        return queryset
-
-
-class NewFlashcardRequestHandler(LoginRequiredMixin, CreateView):
-    model = Flashcard
-    form_class = FlashcardForm
-    template_name = "flashcard/flashcard_create.html"
-    success_url = reverse_lazy("list-flashcards")
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
-
-class DelFlashcardRequestHandler(LoginRequiredMixin, DeleteView):
-    model = Flashcard
-    template_name = "flashcard/flashcard_delete.html"
-    success_url = reverse_lazy("list-flashcards")
+from ..models import Flashcard, Categoria, Desafio, FlashcardDesafio
+from ..forms import DesafioForm
 
 
 class NewChallengeRequestHandler(LoginRequiredMixin, CreateView):
@@ -65,6 +24,14 @@ class NewChallengeRequestHandler(LoginRequiredMixin, CreateView):
 
         if not form.is_valid():
             return redirect(reverse_lazy("challenge_list"))
+
+        if form.instance.quantidade_perguntas > Flashcard.objects.filter(
+                user=self.request.user, dificuldade=form.instance.dificuldade
+            ).count():
+            
+            messages.error(request, "Não há flashcards suficientes")
+            
+            return redirect(reverse_lazy("challenge_create"))
 
         desafio = Desafio(
             user=self.request.user,
@@ -83,9 +50,6 @@ class NewChallengeRequestHandler(LoginRequiredMixin, CreateView):
             categoria_id__in=categorias,
             dificuldade=form.instance.dificuldade,
         ).order_by("?")
-
-        if form.instance.quantidade_perguntas > flashcards.count():
-            return redirect(reverse_lazy("list-flashcards"))
 
         flashcards = flashcards[: form.instance.quantidade_perguntas]
 
@@ -157,31 +121,6 @@ class DelChallengeRequestHandler(LoginRequiredMixin, DeleteView):
     model = Desafio
     template_name = "flashcard/flashcard_delete.html"
     success_url = reverse_lazy("challenge_list")
-
-
-def AwnserFlashcardRequestHandler(request, pk):
-    flashcard_desafio = FlashcardDesafio.objects.get(id=pk)
-
-    if flashcard_desafio.flashcard.user != request.user:
-        return redirect(reverse_lazy("list-flashcards"))
-
-    flashcard_desafio.respondido = True
-    flashcard_desafio.acertou = True if request.GET.get("acertou") == "1" else False
-    flashcard_desafio.save()
-
-    challenge = Desafio.objects.get(id=request.GET.get("desafio_id"))
-    challenge_flashcards = challenge.flashcards.all().count()
-    challenge_flashcards_respondidos = challenge.flashcards.filter(
-        respondido=True
-    ).count()
-
-    if challenge_flashcards_respondidos == challenge_flashcards:
-        challenge.status = True
-        challenge.save()
-
-    return redirect(
-        reverse_lazy("challenge_detail", kwargs={"pk": request.GET.get("desafio_id")})
-    )
 
 
 def RelatoryChallengeRequestHandler(request, pk):
